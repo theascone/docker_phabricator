356be0af370beb6f059f549b3b605c7bbdab988d
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2015-05-05 21:27:36 +0200

    Force storage upgrade

--- VAR_PHABRICATOR_DIR/setup.sh
+++ VAR_PHABRICATOR_DIR/setup.sh
@@ -4,4 +4,4 @@ $PHABRICATOR_DIR/phabricator/bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.user $MYSQL_USER
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.pass $MYSQL_PASS
 
-$PHABRICATOR_DIR/phabricator/bin/storage upgrade
+$PHABRICATOR_DIR/phabricator/bin/storage --force upgrade
