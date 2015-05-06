7a1ec8ea11f0bac11e4264fa89ab151cb8d1e0f2
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2015-05-06 09:12:57 +0200

    Add comments in setup.sh

--- VAR_PHABRICATOR_DIR/setup.sh
+++ VAR_PHABRICATOR_DIR/setup.sh
@@ -1,11 +1,14 @@
+# Set mysql config
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.host $MYSQL_PORT_3306_TCP_ADDR
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
 
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.user $MYSQL_USER
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.pass $MYSQL_PASS
 
+# Upgrade storage
 $PHABRICATOR_DIR/phabricator/bin/storage --force upgrade
 
+# Set base uri
 $PHABRICATOR_DIR/phabricator/bin/config set phabricator.base-uri 'http://'$HOSTNAME
 
 # Set phd and ssh user
