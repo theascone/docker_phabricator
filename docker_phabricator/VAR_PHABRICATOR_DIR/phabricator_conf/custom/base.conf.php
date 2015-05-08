5a72906c8de2810f403ce74b13ec11645bdeba6e
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2015-05-08 19:59:39 +0200

    Fix base config

--- docker_phabricator/VAR_PHABRICATOR_DIR/phabricator_conf/custom/base.conf.php
+++ docker_phabricator/VAR_PHABRICATOR_DIR/phabricator_conf/custom/base.conf.php
@@ -3,7 +3,7 @@
 return array(
     # Set phd and ssh user
     'phd.user' => 'phd',
-    'diffusion.ssh-user' => 'vcs'
+    'diffusion.ssh-user' => 'vcs',
 
     # Set local storage path
     'storage.local-disk.path' => '/var/storage'
