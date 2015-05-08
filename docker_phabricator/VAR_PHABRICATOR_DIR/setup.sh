cafe5ae8e7e2279b7829821f0ed2ae6f4c754390
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2015-05-08 20:19:35 +0200

    Put base config back to setup script

--- docker_phabricator/VAR_PHABRICATOR_DIR/setup.sh
+++ docker_phabricator/VAR_PHABRICATOR_DIR/setup.sh
@@ -7,8 +7,15 @@ $PHABRICATOR_DIR/phabricator/bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.user $MYSQL_USER
 $PHABRICATOR_DIR/phabricator/bin/config set mysql.pass $MYSQL_PASS
 
+# Set phd and ssh user
+$PHABRICATOR_DIR/phabricator/bin/config set phd.user phd
+$PHABRICATOR_DIR/phabricator/bin/config set diffusion.ssh-user  vcs
+
 # Set base uri
 $PHABRICATOR_DIR/phabricator/bin/config set phabricator.base-uri 'http://'$HOSTNAME
+
+# Set local storage path
+$PHABRICATOR_DIR/phabricator/bin/config set storage.local-disk.path /var/storage
 
 # Upgrade storage
 $PHABRICATOR_DIR/phabricator/bin/storage --force upgrade
