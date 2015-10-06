bfe5629f16cf7b66dd1732956ac867738d0fa699
Craeckie <josia-login@rosskopfs.de>
2015-10-06 12:35:07 +0200

    Use mysql link-ENV
    Use $MYSQL_USER and $MYSQL_PASS from link environment, if not passed as a local ENV (-e).

--- opt/phabricator/setup.sh
+++ opt/phabricator/setup.sh
@@ -4,8 +4,24 @@
 /opt/phabricator/phabricator/bin/config set mysql.host mysql
 /opt/phabricator/phabricator/bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
 
-/opt/phabricator/phabricator/bin/config set mysql.user $MYSQL_USER
-/opt/phabricator/phabricator/bin/config set mysql.pass $MYSQL_PASS
+if [ -n "$MYSQL_USER" ]; then
+  /opt/phabricator/phabricator/bin/config set mysql.user $MYSQL_USER
+elif [ -n "$MYSQL_ENV_MYSQL_USER"]; then
+  # use link environment
+  /opt/phabricator/phabricator/bin/config set mysql.user $MYSQL_ENV_MYSQL_USER
+else
+  echo "Error: \$MYSQL_USER is not set and not found in link environment! (\$MYSQL_ENV_MYSQL_USER)" 1>&2
+  exit(1)
+fi
+if [ -n "$MYSQL_PASS" ]; then
+  /opt/phabricator/phabricator/bin/config set mysql.pass $MYSQL_PASS
+elif [ -n "$MYSQL_ENV_MYSQL_PASS"]; then
+  # use link environment
+  /opt/phabricator/phabricator/bin/config set mysql.pass $MYSQL_ENV_MYSQL_PASS
+else
+  echo "Error: \$MYSQL_PASS is not set and not found in link environment! (\$MYSQL_ENV_$MYSQL_PASS)" 1>&2
+  exit(1)
+fi
 
 # Set phd and ssh user
 /opt/phabricator/phabricator/bin/config set phd.user phd
