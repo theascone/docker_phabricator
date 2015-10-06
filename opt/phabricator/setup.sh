b0ca3dbf126a9b007fdbe617f5729c0db485ff63
Craeckie <josia-login@rosskopfs.de>
2015-10-06 12:47:05 +0200

    function for setting config values
    Wrapped the decision of using local v. link ENV and the set of the config in a function.

--- opt/phabricator/setup.sh
+++ opt/phabricator/setup.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 
+function setVar() { 
+  local conf_name="$1" local_name="$2" local_val="$3" link_name="$4" link_val="$5"  
+  if [ -n "$local_val" ]; then
+  /opt/phabricator/phabricator/bin/config set "$conf_name" "$local_val"
+elif [ -n "$link_val"]; then
+  # use link environment
+  /opt/phabricator/phabricator/bin/config set "$conf_name" "$link_val"
+else
+  echo "Error: $local_name is not set and not found in link environment! ($link_name)" 1>&2
+  exit(1)
+fi
+}
+
 # Set mysql config
 /opt/phabricator/phabricator/bin/config set mysql.host mysql
 /opt/phabricator/phabricator/bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
+#setVar mysql.user "\$MYSQL_PORT" "$MYSQL_USER" "\$MYSQL_ENV_MYSQL_USER" "$MYSQL_ENV_MYSQL_USER"
 
-if [ -n "$MYSQL_USER" ]; then
-  /opt/phabricator/phabricator/bin/config set mysql.user $MYSQL_USER
-elif [ -n "$MYSQL_ENV_MYSQL_USER"]; then
-  # use link environment
-  /opt/phabricator/phabricator/bin/config set mysql.user $MYSQL_ENV_MYSQL_USER
-else
-  echo "Error: \$MYSQL_USER is not set and not found in link environment! (\$MYSQL_ENV_MYSQL_USER)" 1>&2
-  exit(1)
-fi
-if [ -n "$MYSQL_PASS" ]; then
-  /opt/phabricator/phabricator/bin/config set mysql.pass $MYSQL_PASS
-elif [ -n "$MYSQL_ENV_MYSQL_PASS"]; then
-  # use link environment
-  /opt/phabricator/phabricator/bin/config set mysql.pass $MYSQL_ENV_MYSQL_PASS
-else
-  echo "Error: \$MYSQL_PASS is not set and not found in link environment! (\$MYSQL_ENV_$MYSQL_PASS)" 1>&2
-  exit(1)
-fi
+setVar mysql.user "\$MYSQL_USER" "$MYSQL_USER" "\$MYSQL_ENV_MYSQL_USER" "$MYSQL_ENV_MYSQL_USER"
+setVar mysql.pass "\$MYSQL_PASS" "$MYSQL_PASS" "\$MYSQL_ENV_MYSQL_PASS" "$MYSQL_ENV_MYSQL_PASS"
 
 # Set phd and ssh user
 /opt/phabricator/phabricator/bin/config set phd.user phd
