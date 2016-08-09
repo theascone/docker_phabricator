ea05ea8acd3ef0e52aad0e2ce9c28a735bb6f3c3
Josia <josia-internet@rosskopfs.de>
2016-08-09 20:01:18 +0200

    only change ssh-conf when necessary

--- opt/setup.sh
+++ opt/setup.sh
@@ -5,17 +5,28 @@ pushd "/etc/ssh/" >dev/null
 if ! [ -f "ssh_host_rsa_key" ]; then
   echo -e "Regenerating ssh keys.\nKeep /etc/ssh/ as a volume to make it persistent."
   rm ssh_host_* 2>/dev/null
-  dpkg-reconfigure openssh-server
-  echo "Regenerating DH primes, this will take a few minutes.. "
-  ssh-keygen -G /etc/ssh/moduli
+  dpkg-reconfigure openssh-server || exit
   echo "Done!"
-  echo "Installing sshd-config.. "
-  cp /opt/phabricator/phabricator/resources/sshd/sshd_config.phabricator.example /etc/ssh/sshd_config.phabricator \  
-      && sed -i "s|^AuthorizedKeysCommandUser\s.*|AuthorizedKeysCommandUser vcs|g" /etc/ssh/sshd_config.phabricator \  
-      && sed -i "s|^AllowUsers\s.*|AllowUsers vcs|g" /etc/ssh/sshd_config.phabricator
+fi
+if ! [ -f "/etc/ssh/moduli" ]; then
+  echo "Regenerating DH primes, this will take a few minutes.. "
+  ssh-keygen -G /etc/ssh/moduli || exit
+  echo "Done!"
+fi
 
-  [ -f "/etc/ssh_config" ] || cp /tmp/ssh_config /etc/ssh_config
-  
+ssh_conf_phab="sshd_config.phabricator"
+if ! [ -f "$ssh_conf_phab" ]; then
+  echo -n "Installing phabricator sshd-config.. "
+  cp /opt/phabricator/phabricator/resources/sshd/sshd_config.phabricator.example "$ssh_conf_phab" || exit
+  sed -i "s|^AuthorizedKeysCommandUser\s.*|AuthorizedKeysCommandUser vcs|g" "$ssh_conf_phab" || exit
+  sed -i "s|^AllowUsers\s.*|AllowUsers vcs|g" "$ssh_conf_phab" || exit
+  echo "Done!"
+fi
+
+ssh_conf="/etc/ssh/ssh_config"
+if ! [ -f "$ssh_conf" ]; then
+  echo -e "Installing sshd-config.. "
+  cp /tmp/ssh_config "$ssh_conf" || exit
   echo "Done!"
 fi
 popd >/dev/null
